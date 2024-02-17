5ddc25d18e1d2407228e49e5b93b6e79aee70c01
Gareth Simpson <g@xurble.org>
2024-02-17 23:42:10 +0000

    Preview image widened

--- rc/middleware.py
+++ rc/middleware.py
@@ -10,16 +10,16 @@ class PreviewMiddleware:
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        
+
         request.vals = {
-            "page_url" : settings.FEEDS_SERVER + request.META["PATH_INFO"], 
+            "page_url" : settings.FEEDS_SERVER + request.META["PATH_INFO"],
             "site_name" : "Recast",
-            "page_image" : settings.FEEDS_SERVER + "/static/images/recast-large.png",
+            "page_image" : settings.FEEDS_SERVER + "/static/images/recast-preview-image.png",
             "page_title" : "The Simple Podcast Catch-Up Service",
             "page_description" : "Recast is a simple service that will replay all the episodes of a podcast from the start.",
         }
-        
-        
+
+
 
 
         response = self.get_response(request)
@@ -27,5 +27,5 @@ class PreviewMiddleware:
         # Code to be executed for each request/response after
         # the view is called.
 
-        
+
         return response
