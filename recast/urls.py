d59a5a46fcf16deab4e66a5024159e08aeb61730
Gareth Simpson <g@xurble.org>
2024-03-04 05:36:38 +0000

    Add test source option to the feedgarden.

--- recast/urls.py
+++ recast/urls.py
@@ -1,11 +1,26 @@
-from django.urls import path, include
 
-
+from django.urls import path
+from rc.views import (
+    index,
+    reader,
+    help,
+    editfeed,
+    feed,
+    revivesource,
+    testsource,
+    subscribe,
+    source,
+    post_redirect,
+    enclosure_redirect,
+    addfeed,
+    feedgarden,
+    robots,
+    favicon
+)
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-from rc.views import *
 
 urlpatterns = [
     # Examples:
@@ -23,31 +38,28 @@ urlpatterns = [
     path(r'refresh/', reader),
     path(r'help/', help),
 
-    path('feed/(<str:key>)/edit/', editfeed), # legacy
+    path('feed/(<str:key>)/edit/', editfeed),  # legacy
     path('feed/edit/<str:key>/', editfeed, name='editfeed'),
     path('feed/<str:key>/', feed, name='feed'),
 
     path('source/<int:sid>/revive/', revivesource),
+    path('source/<int:sid>/test/', testsource),
     path('source/<int:sid>/subscribe/', subscribe, name='subscribe'),
     path('source/<int:sid>/', source, name='source'),
-
-    
-
 
     path('post/<int:pid>/', post_redirect),
     path('enclosure/<int:eid>/', enclosure_redirect),
 
 
     path('addfeed/', addfeed),
-    
+
 
     path('feedgarden/', feedgarden),
 
-    
+
 
     path('robots.txt', robots),
     path('favicon.ico', favicon),
 
-    
-]
 
+]
